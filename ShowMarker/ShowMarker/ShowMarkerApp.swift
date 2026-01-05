bb0a88aa529d886bb7cbd5897716ca237060be5d
peterpluz <peter.pluz.personal@gmail.com>
2026-01-05 12:19:03 +0300

    Add base architecture structure

---
+++ ShowMarker/ShowMarker/ShowMarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowMarkerApp.swift
+//  ShowMarker
+//
+//  Created by Пётр Гущин on 05.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ShowMarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
